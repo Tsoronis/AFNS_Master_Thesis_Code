adba3ea41f4afcdd4a32c5ac4b38ef57942bd395
Tsoronis <60613992+Tsoronis@users.noreply.github.com>
2022-05-28 12:26:39 +0200

    Update constrained_guesser.py
    Updated comments

--- constrained_guesser.py
+++ constrained_guesser.py
@@ -8,19 +8,22 @@ import random
 def random_constrained_geuss(t):
     random_float_list = []
     for i in range(0,3):
-        x = round(random.uniform(0.08, 1.0), 5)
+        x = round(random.uniform(0.01, 1.0), 5) # Kappa matrix diagonal values
         random_float_list.append(x) 
     for i  in range(0,3):
-        x = round(random.uniform(0.001, 0.005), 5)
-        random_float_list.append(x)
-    for i  in range(0,3):
-        x = round(random.uniform(-0.05, 0.08), 5)
+        x = round(random.uniform(0.001, 0.005), 5)  # Sigma matrix diagonal values
         random_float_list.append(x)
     for i  in range(0,1):
-        x = round(random.uniform(0.5, 0.6), 5)
+        x = round(random.uniform(0.01, 0.05), 5) # Theta_1 
+        random_float_list.append(x)
+    for i  in range(0,2):
+        x = round(random.uniform(-0.05, -0.01), 5) # Theta_2 and Theta_3
+        random_float_list.append(x)
+    for i  in range(0,1):
+        x = round(random.uniform(0.1, 0.9), 5) # Lambda
         random_float_list.append(x)  
     for i  in range(0,len(t)):                  
-        x = round(random.uniform(0.0001, 0.0009), 5)
+        x = round(random.uniform(0.0001, 0.0009), 5) # Random guees for the range..
         random_float_list.append(x) 
     return random_float_list    
 
